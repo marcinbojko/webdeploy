c4666c391692bb1702d395768d4f466af30da2e1
Marcin Bojko <marcinbojko.pl@gmail.com>
2017-04-19 14:55:49 +0200

    2017-04-19 Build 3.6.0

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,80 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$packageName        = 'webdeploy'
+$scriptPath         = $(Split-Path $MyInvocation.MyCommand.Path)
+$url_local          = "https://download.microsoft.com/download/0/1/D/01DC28EA-638C-4A22-A57B-4CEF97755C6C/WebDeploy_x86_en-US.msi"
+$url_remote         = "https://download.microsoft.com/download/0/1/D/01DC28EA-638C-4A22-A57B-4CEF97755C6C/WebDeploy_x86_en-US.msi"
+$url_local64        = "https://download.microsoft.com/download/0/1/D/01DC28EA-638C-4A22-A57B-4CEF97755C6C/WebDeploy_amd64_en-US.msi"
+$url_remote64       = "https://download.microsoft.com/download/0/1/D/01DC28EA-638C-4A22-A57B-4CEF97755C6C/WebDeploy_amd64_en-US.msi"
+$url_local_trans    = ""
+$url_remote_trans   = ""
+$url                = ""
+$url_trans          = ""
+$checksum           = "0aaee0b66455c2b1e51d07e657960fbf1ae8ba65314ef79f9001f64cff689001"
+$checksum64         = "c0369a7dc11a3faa78a12ce77ea393d9fe75c44273ba532cbf9c2e1fe5ee1768"
+$logfile            = "$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log"
+$logdir             = "$env:TEMP\chocolatey\$($packageName)"
+$killexec           = 0
+$killexecprocess    = ""
+# Let's check if should we use local or remote install source
+$statusCode = Test-Path $url_local
+if ($statusCode) {
+                    $url=$url_local
+                    $url_trans=$url_local_trans
+                }
+    else {
+        $url=$url_remote
+        $url_trans=$url_remote_trans
+    }
+# Someone has won ;)
+
+
+
+#Let's check your TEMP derectory
+
+$statusCode = Test-Path $logdir
+if ($statusCode) {
+
+                }
+    else {
+        $logfile="$env:WINDIR\TEMP\chocolatey\$($packageName).MsiInstall.log"
+
+    }
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'msi'
+  silentArgs    = "/qn /norestart ADDLOCAL=ALL"
+  validExitCodes= @(0, 3010, 1603, 1641)
+  url           = $url
+  url64bit      = $url_remote64
+  checksumType  = 'sha256'
+  checksumType64= 'sha256'
+  checksum      = $checksum
+  checksum64    = $checksum64
+}
+
+# Should we kill some exec ?
+if ($killexec) {
+  Stop-Process -processname $killexecprocess -force
+  }
+
+  Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
